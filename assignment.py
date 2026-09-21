ed0271682c64cba3dfe1e2e930526d3856a90ea4
khorloo-afk <khorolmaa.kh2016@orkhonschool.edu.mn>
2026-09-21 09:01:59 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,24 +2,35 @@
 
 # Exercise 1
 def count_characters(text):
-    # Write your code here
+    return len(text)
+    
+print(count_characters("Hello"))
     pass
 
 # Exercise 2
 def remove_spaces(text):
-    # Write your code here
+i = input()
+print(len(s.replace(" ", "")))
     pass
 
 # Exercise 3
 def count_vowels(text):
-    # Write your code here
-    pass
+text = "Hello World"
+vowels = "aeiou"
+count = 0
+for c in text.lower():
+    if c in vowels:
+        count += 1
 
+print(count)
 # Exercise 4
 def replace_vowels(text):
-    # Write your code here
-    pass
+text = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+for vowel in vowels:
+    text = text.replace(vowel, "*")
 
+print(text)
 # Exercise 5
 def count_words(text):
     # Write your code here
@@ -29,3 +40,18 @@ def count_words(text):
 def find_longest_word(text):
     # Write your code here
     pass
+#7
+# DECLARE Num:INTEGER
+# Num<-INT(RAND(100))+1
+# OUTPUT "Enter num between 1 to 100"
+# INPUT UserGuess
+# WHILE UserGuess<>Num DO
+#     IF UserGuess<Num THEN
+#         OUTPUT "TOO LOW Enter again"
+#     ELSE
+#         OUTPUT "TOO High Enter again"
+#     ENDIF
+#     INPUT UserGuess
+# ENDWHILE
+# OUTPUT "You win"
+
